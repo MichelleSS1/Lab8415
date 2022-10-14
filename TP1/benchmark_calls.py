23a8fbfc078ee8785a946d21bbb71163df0161c3
MichelleSS <simemichelle01@gmail.com>
2022-10-14 17:17:48 -0400

    Renamed file

--- TP1/benchmark_calls.py
+++ TP1/benchmark_calls.py
@@ -4,15 +4,17 @@ from time import sleep
 import requests
 
 # Load balancer DNS name to use in the url
-LB_DNS_NAME = os.environ.get('LB_DNS_NAME')
+#LB_DNS_NAME = os.environ.get('LB_DNS_NAME')
+LB_DNS_NAME = ""
 
 CLUSTER1_PATH = '/cluster1'
 CLUSTER2_PATH = '/cluster2'
 
 
-def call_endpoint(path):
+def call_endpoint(path:str):
     '''
     Calls (GET request) the load balancer on the given path.
+    @param path:str Path to call
     '''
 
     url = LB_DNS_NAME + path
@@ -25,21 +27,21 @@ def call_endpoint(path):
     print(f'Code : {res.status_code}, Text: {res.json()}')
 
 
-def make_calls(nb, path):
+def make_calls(nb:int, path:str):
     '''
     Make `nb` calls sequentially to `path`.
-    nb: number of calls to make
-    path: Path to call
+    @param nb:int number of calls to make
+    @param path:str Path to call
     '''
     
     for i in range(nb):
         call_endpoint(path)
     
 
-def first_thread_calls(path):
+def first_thread_calls(path:str):
     '''
     First test function (will make the first test thread).
-    path: Path to call
+    @param path:str Path to call
     '''
 
     print("Started first test thread for path :", path)
@@ -50,10 +52,10 @@ def first_thread_calls(path):
     print("Exited first test thread for path :", path)
 
 
-def second_thread_calls(path):
+def second_thread_calls(path:str):
     '''
     Second test function (will make the second test thread).
-    path: Path to call
+    @param path:str Path to call
     '''
 
     print("Started second test thread for path :", path)
@@ -70,10 +72,12 @@ def second_thread_calls(path):
     print("Exited second test thread for path :", path)
 
 
-def create_test_threads(path):
+def create_test_threads(path:str):
     '''
     This function creates the test threads needed given the path to call.
-    path: Path to call
+    @param path:str Path to call
+
+    @returns test threads objects
     '''
 
     print("Creating test threads for path :", path)
@@ -89,15 +93,17 @@ def create_test_threads(path):
 
 if __name__ == '__main__':
     print("Starting Tests")
+    make_calls(5, CLUSTER1_PATH)
+    make_calls(5, CLUSTER2_PATH)
 
-    # Creating test threads for both clusters.
-    # We keep the threads to join them (make sure they end their execution) before shutdown.
-    threads = []
-    threads.extend(create_test_threads(CLUSTER1_PATH))
-    threads.extend(create_test_threads(CLUSTER2_PATH))
+    # # Creating test threads for both clusters.
+    # # We keep the threads to join them (make sure they end their execution) before shutdown.
+    # threads = []
+    # threads.extend(create_test_threads(CLUSTER1_PATH))
+    # threads.extend(create_test_threads(CLUSTER2_PATH))
 
-    # Join all threads before exiting.
-    for thread in threads:
-        thread.join()
+    # # Join all threads before exiting.
+    # for thread in threads:
+    #     thread.join()
 
     print("Completed tests!")
