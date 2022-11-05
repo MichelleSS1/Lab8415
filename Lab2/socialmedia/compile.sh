36f4884ff8dfb5c4a5fb642623f45872da29bc0b
Rui Jie Li <rui-jie.li@polymtl.ca>
2022-11-04 23:52:14 -0400

    added extra explanation

--- Lab2/socialmedia/compile.sh
+++ Lab2/socialmedia/compile.sh
@@ -1,3 +1,5 @@
+# code based on
+# Apache Software Foundation. (2022, July 29). Mapreduce tutorial. Apache Hadoop. Retrieved November 4, 2022, from https://hadoop.apache.org/docs/stable/hadoop-mapreduce-client/hadoop-mapreduce-client-core/MapReduceTutorial.html 
 rm -r /home/ruijieli/Desktop/output
 export JAVA_HOME="/usr/lib/jvm/java-17-openjdk-17.0.4.1.1-1.fc36.x86_64"
 export PATH=${JAVA_HOME}/bin:${PATH}
